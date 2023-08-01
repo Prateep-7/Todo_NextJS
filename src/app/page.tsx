68038c58f9a3b2ad2f7a31c7142c7db5eae015c1
Prateep Nitheyasankar <prateep.nitheyasankar@bounteous.com>
2023-08-01 15:58:49 +0530

    change the icon name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home(){
       <Link 
       className="border border-slate-300 text-slate-300 px-2 py-1 rounded
       hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-      href="/new">New Page</Link>
+      href="/new">New</Link>
     </header>
   </>
 }
